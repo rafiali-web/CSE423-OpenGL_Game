394a57f5e36dfb19a1324b153ef1468f3a0ef101
Rafi_@li <rafi.ali@g.bracu.ac.bd>
2025-12-28 01:00:30 +0600

    Update Project_Body.py tar

--- Project_Body.py
+++ Project_Body.py
@@ -9,6 +9,13 @@ print("new rafi")
 print("rafi was here")
 print ("rafi was NO here")
 print("YASIN WAS HERE")
+bullets = []
+cheat_ball = None
+cheat_ball_timer = 0
+cheat_ball_duration = 5
+cheat_ball_pulse = 5.0  # Added fattening effect like code 2
+enemies_distracted = False
+
 
 
 print(10+10)
